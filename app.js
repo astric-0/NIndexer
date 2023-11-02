a2e65606abfc4bb1ee87575aa4e0b624e947b07c
Nittin <nittin.sohalia@bettercommerce.io>
2023-11-02 17:31:12 +0530

    changed port number

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ import IndexerDatabase from "./lib/IndexerDatabase.js";
 IndexerDatabase();
 
 const app = express();
-const port = 3003;
+const port = 3001;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/client', clientRouter);
+app.use('/', (_, res) => res.send('Indexer IsActive'));
 app.use(errorHandler);
 
 app.listen(port, error => {
